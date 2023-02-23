2dbbd6992d259cd3177cce2e74afcc75387f47ae
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-02-23 09:44:24 +0600

    chat-content component created on chat page

---
+++ app/Http/Livewire/Backend/Chat/ChatContent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Backend\Chat;
+
+use Livewire\Component;
+
+class ChatContent extends Component
+{
+    public function render()
+    {
+        return view('livewire.backend.chat.chat-content');
+    }
+}
