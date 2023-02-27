98d9b803ee4842f4bccf04bf6a541516cb52e742
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-02-27 17:42:52 +0600

    conversation module created

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Livewire\Backend\Chat\Chat;
 
 
 ## Backend routes
-Route::prefix('admin')->name('admin.')->group(function () {
+Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/chat', Chat::class)->name('chat');
 });
