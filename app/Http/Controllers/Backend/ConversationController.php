e2627956b9dad5b9c0bf68cd287b7f01b1750396
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-03-01 00:32:22 +0600

    optimized conversation controller

--- app/Http/Controllers/Backend/ConversationController.php
+++ app/Http/Controllers/Backend/ConversationController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Contracts\View\View;
 
 class ConversationController extends Controller
@@ -17,53 +14,5 @@ class ConversationController extends Controller
     public function index(): View
     {
         return view('livewire.backend.chat.chat');
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create(): Response
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request): RedirectResponse
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id): Response
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id): Response
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id): RedirectResponse
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id): RedirectResponse
-    {
-        //
     }
 }
