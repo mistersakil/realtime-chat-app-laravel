612cc86ece368e6241e525bdd9bca04f8cfe98e5
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-03-02 15:23:14 +0600

    working on chat list users

--- app/Http/Livewire/Backend/Chat/ChatList.php
+++ app/Http/Livewire/Backend/Chat/ChatList.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Livewire\Backend\Chat;
 
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class ChatList extends Component
 {
-    public function render()
+    public function render(): View
     {
         return view('livewire.backend.chat.chat-list');
     }
