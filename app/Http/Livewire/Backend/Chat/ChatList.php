5e899e40a3facdaeed8ff22287fe6386dd9d661e
Sakil Mahmud <sakil.diu.cse@gmail.com>
2023-03-03 19:05:02 +0600

    fetch all users on chatlist component

--- app/Http/Livewire/Backend/Chat/ChatList.php
+++ app/Http/Livewire/Backend/Chat/ChatList.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Livewire\Backend\Chat;
 
-use Illuminate\Contracts\View\View;
+use App\Models\User;
 use Livewire\Component;
+use Illuminate\Contracts\View\View;
 
 class ChatList extends Component
 {
+    public User $users;
+
+    public function mound():void{
+        $this->users = User::get();
+    }
     public function render(): View
     {
         return view('livewire.backend.chat.chat-list');
